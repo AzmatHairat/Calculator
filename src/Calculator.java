48b512a9e73f26d6fe15ab09d59b8e10d19d78e8
Azmat <azmat1816@gmail.com>
2019-01-19 22:33:59 -0500

    This is v1.3 Tonight

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    //This is a very useful method.
+    //This is a very useful method. V1.3
 
     public static double add (double num1 , double num2){
         int testNG = 1+1;
@@ -18,9 +18,6 @@ public class Calculator {
         return num1 * num2;
     }
     
-    public static double divide(double num1, double num2){
-        return num1 / num2;
-    }
 
     public static double divide(double nu1, double nu2){
         double result = nu1 /nu2;
