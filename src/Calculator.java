5ea0ea206c75599249f118fb8f907c36d6f1e1c6
Azmat <azmat1816@gmail.com>
2019-01-19 22:44:37 -0500

    what happen

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    //This is a very useful method. V1.4
+    //This is a very useful method. V1.3
 
     public static double add (double num1 , double num2){
         int testNG = 1+1;
@@ -12,18 +12,20 @@ public class Calculator {
 
         return result ;
     }
-    
+
     public static double multiply(double num1,double num2){
-    
+
         return num1 * num2;
     }
-    
 
-    public static double divide(double num1, double num2){
-        return num1 / num2;
+
+    public static double divide(double nu1, double nu2){
+        double result = nu1 /nu2;
+        return result;
     }
 
 
 
 }
+
 
